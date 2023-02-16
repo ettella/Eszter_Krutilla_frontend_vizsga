4292563685caf569874dc55b3624543167974283
ettella <ettella@hotmail.com>
2023-02-16 12:05:50 +0100

    add form

--- src/app/new-plushie/new-plushie.component.ts
+++ src/app/new-plushie/new-plushie.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Plushie } from '../plushie/plushie-model';
 
 @Component({
   selector: 'app-new-plushie',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-plushie.component.scss']
 })
 export class NewPlushieComponent {
+
+  plushes: Plushie[] = [];
+  plushForm: FormGroup;
+  isSubmit: boolean = false;
+
+  constructor(public fb: FormBuilder, 
+    ) {
+    this.plushForm = fb.group(
+      { 
+        name: ['', Validators.required],
+        size: [Number, Validators.required],
+        
+      }
+    );
+  };
+
+  get name():FormControl {
+    return this.plushForm.get('name') as FormControl;
+  };
+
+  get size():FormControl {
+    return this.plushForm.get('size') as FormControl;
+  };
+
+  onSubmit(plush:Plushie){
+    this.plushes.push(plush);
+    this.isSubmit = true;
+    this.plushForm.reset();
+    console.log('submit button works',this.plushForm.value)
+  }
 
 }
